919bf5cbcfcb1985ef440d0d2248ebded8d8508f
dominiczhu <dominiczhu@tencent.com>
2022-01-30 16:37:29 +0800

    p57

--- atguigu2020/cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ atguigu2020/cloud2020/cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -27,12 +27,12 @@ public class PaymentService
     }
 
     @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="3000")
+            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="5000")
     })
     public String paymentInfo_TimeOut(Integer id)
     {
-        int age = 10/0;
-//        try { TimeUnit.MILLISECONDS.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
+//        int age = 10/0;
+        try { TimeUnit.MILLISECONDS.sleep(3000); } catch (InterruptedException e) { e.printStackTrace(); }
         return "线程池:  "+Thread.currentThread().getName()+" id:  "+id+"\t"+"O(∩_∩)O哈哈~"+"  耗时(秒): ";
     }
     public String paymentInfo_TimeOutHandler(Integer id)
